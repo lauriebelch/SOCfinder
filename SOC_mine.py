49b767f099955638c56497532350fbbd8c44c235
lauriebelch <laurencebelcher@googlemail.com>
2023-05-15 16:13:07 +0100

    update files

--- SOC_mine.py
+++ SOC_mine.py
@@ -36,30 +36,6 @@ fasta = args.FASTAinput
 gff = args.gffinput
 OUTPUT = args.KOFAMoutput
 
-
-### include an option for building blast databases
-## a flag called m that determines if the databases must be made
-#fasta_folder = "/Users/laurie/Dropbox/POSTDOC/PROJECT_3_SOCIAL_GENES/psort_simple"
-#database_folder = "blast_databases"
-
-#bash_script = """#!/bin/bash
-#mkdir blast_databases
-#makeblastdb -in {ff}/psort_extracellular_gramP.fasta -parse_seqids -dbtype prot -out {db}/blastdbP &
-#makeblastdb -in {ff}/psort_extracellular_gramN.fasta -parse_seqids -dbtype prot -out {db}/blastdbN &
-#makeblastdb -in {ff}/EXTRA.fasta -parse_seqids -dbtype prot -out {db}/blastdbCExtra &
-#makeblastdb -in {ff}/NON_EXTRA.fasta -parse_seqids -dbtype prot -out {db}/blastdbCNonExtra
-#"""
-# Format the bash script with the input and output filenames
-#formatted_script1 = bash_script.format(db=database_folder, ff=fasta_folder)
-# Create a temporary file to store the bash script
-#script_filename1 = 'makedb.sh'
-#with open(script_filename1, 'w') as script_file:
- #   script_file.write(formatted_script1)
-# Execute the bash script
-# only run this line if you need to build the databases
-###subprocess.run(['bash', script_filename1], check=True)
-
-
 ######################################################################
 ################### Make bash script ##################################
 ######################################################################
@@ -118,9 +94,4 @@ subprocess.run(['bash', script_filename], check=True)
 
 # Remove the temporary script file
 #subprocess.run(['rm', script_filename])
-
-
-
-
-
 
