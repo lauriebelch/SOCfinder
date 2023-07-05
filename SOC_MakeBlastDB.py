2c24b7487141dbf33c095fc739aae2cefadeb34c
lauriebelch <laurencebelcher@googlemail.com>
2023-07-05 09:41:35 +0100

    Update SOC_MakeBlastDB.py

--- SOC_MakeBlastDB.py
+++ SOC_MakeBlastDB.py
@@ -13,9 +13,9 @@ database_folder = "blast_databases"
 
 bash_script = """#!/bin/bash
 mkdir blast_databases
-makeblastdb -in {ff}/psort_extracellular_gramP.fasta -parse_seqids -dbtype prot -out {db}/blastdbP &
-makeblastdb -in {ff}/psort_extracellular_gramN.fasta -parse_seqids -dbtype prot -out {db}/blastdbN &
-makeblastdb -in {ff}/EXTRA.fasta -parse_seqids -dbtype prot -out {db}/blastdbCExtra &
+makeblastdb -in {ff}/psort_extracellular_gramP.fasta -parse_seqids -dbtype prot -out {db}/blastdbP
+makeblastdb -in {ff}/psort_extracellular_gramN.fasta -parse_seqids -dbtype prot -out {db}/blastdbN
+makeblastdb -in {ff}/EXTRA.fasta -parse_seqids -dbtype prot -out {db}/blastdbCExtra
 makeblastdb -in {ff}/NON_EXTRA.fasta -parse_seqids -dbtype prot -out {db}/blastdbCNonExtra
 """
 # Format the bash script with the input and output filenames
